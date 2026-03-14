e450f2c45dde5af47c505e91ef72e73e2a001c94
adnan <126405134+AdnanISB@users.noreply.github.com>
2026-03-14 11:00:10 +0000

    files added

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -23,7 +23,7 @@ resource "local_file" "private_key" {
 } 
 
 # Call EC2 module (without count.index)
-module "dev_ec2" {
+module "frontend_ec2" {
   source         = "../../modules/ec2"
   environment    = var.environment
   ami            = var.ami
@@ -31,10 +31,59 @@ module "dev_ec2" {
   instance_type  = var.instance_type
   ec2_username   = var.ec2_username
   key_name       = aws_key_pair.default.key_name  # pass actual key name
-  ec2_sg         = var.ec2_sg
+  ec2_sg         = "frontend-${var.environment}"
   ssh_port       = var.ssh_port
-  tags           = var.tags  # pass plain tags
+  tags           = {
+    Name = "Frontend-${var.environment}"
+    Environment = "dev"
+  }  # pass plain tags
+  root_volume_size = 10
+  root_volume_type = var.root_volume_type
+
+  # Optional: pass provider if module defines its own
+  providers = {
+    aws = aws
+  }
+}
+
+module "backend_ec2" {
+  source         = "../../modules/ec2"
+  environment    = var.environment
+  ami            = var.ami
+  instance_count = var.instance_count
+  instance_type  = var.instance_type
+  ec2_username   = var.ec2_username
+  key_name       = aws_key_pair.default.key_name  # pass actual key name
+  ec2_sg         = "backend-${var.environment}"
+  ssh_port       = var.ssh_port
+  tags           = {
+    Name = "Backend-${var.environment}"
+    Environment = "dev"
+  }  # pass plain tags
   root_volume_size = var.root_volume_size
+  root_volume_type = var.root_volume_type
+
+  # Optional: pass provider if module defines its own
+  providers = {
+    aws = aws
+  }
+}
+
+module "database_ec2" {
+  source         = "../../modules/ec2"
+  environment    = var.environment
+  ami            = var.ami
+  instance_count = 2
+  instance_type  = var.instance_type
+  ec2_username   = var.ec2_username
+  key_name       = aws_key_pair.default.key_name  # pass actual key name
+  ec2_sg         = "database-${var.environment}"
+  ssh_port       = var.ssh_port
+  tags           = {
+    Name = "Database-${var.environment}"
+    Environment = "dev"
+  }  # pass plain tags
+  root_volume_size = 20
   root_volume_type = var.root_volume_type
 
   # Optional: pass provider if module defines its own
